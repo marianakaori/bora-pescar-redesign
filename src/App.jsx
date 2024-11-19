0072de7d3b914d297a8a179dd288d42024f989fd
EduardoMirandaz <eduardo.cmazevedo@gmail.com>
2024-11-19 18:00:50 -0300

    Adicionando botão de acessibilidade para ir ao topo da página.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from "./components/Header/header";
 import Home from "./pages/Home/home";
 import Contact from "./pages/Contact/contact";
 import Footer from "./components/Footer/footer";
+import JumpToStartArrow from "./components/JumpToStartArrow/jumpToStartArrow";
+
 
 function App() {
     return (
@@ -12,6 +14,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
+            <JumpToStartArrow/>
             <Footer />
         </Router>
     );
